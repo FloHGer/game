8b5cb574ddd9f7ac16305334df62f9f6c4b53e8c
FloHGer <mail@FlorianHoehle.de>
2021-03-16 22:36:00 +0100

    interactions outsourced

--- script.js
+++ script.js
@@ -4,38 +4,21 @@ var bagContent = ["stone"];
 var dungeonOptions = ["a pile of ruins.", "a dirty sheet.", "the door.", "Azruk."];
 var dungeon2Options = ["a skeleton.", "the tunnel.", "a spoon.", "a key."];
 
-function cookie(){
-    if(document.cookie == "user=" || document.cookie == "user=null"){
-        var user = prompt("Enter your name: ");
-        document.cookie = "user=" + user;
-        cookie();
-        return;
-    }else{
-        document.getElementById("output").innerHTML = document.cookie;
-        view.dungeon(0);
-        return;
-    }
-}
-
-function cookieReset(){
-    document.cookie = "user=";
-}
 
 // keyboard input
 document.onkeydown = checkKey;
 function checkKey(k){
-
     switch(k.keyCode){
-    case 38:
     // up arrow - interact
+    case 38:
         if(bagStatus == "closed"){
-            window["view"][currScene](10);
+            window["interact"][currScene]();
         }else if(bagStatus == "open"){
             bagInteraction();
         }
         break;
-    case 37 :
     // left arrow
+    case 37 :
         if(bagStatus == "closed"){
             document.getElementById("info").innerHTML = "";
             window["view"][currScene](-1);
@@ -43,8 +26,8 @@ function checkKey(k){
             bag(-1);
         }
         break;
-    case 39:
     // right arrow
+    case 39:
         if(bagStatus == "closed"){
             document.getElementById("info").innerHTML = "";
             window["view"][currScene](1);
@@ -52,19 +35,18 @@ function checkKey(k){
             bag(1);
         }
         break;
-    case 40:
     // down arrow - bag open/close
+    case 40:
         bagOpen();
         break;
     default:
     }
 }
 
-
 // bag
 function bagOpen(){
-    if(bagStatus == "closed"){
     // open bag
+    if(bagStatus == "closed"){
         document.getElementById("bagLabel").innerHTML = "Bag: ";
         if(bagContent.length === 0){
             bagStatus = "empty";
@@ -74,8 +56,8 @@ function bagOpen(){
             bagItem = 0;
             document.getElementById("bag").innerHTML = bagContent[0];
         }
-    }else{
     // close bag
+    }else{
         bagStatus = "closed";
         document.getElementById("bagLabel").innerHTML = "";
         document.getElementById("bag").innerHTML = "";
@@ -99,12 +81,13 @@ function bag(i){
     return document.getElementById("bag").innerHTML = bagContent[bagItem];
 }
 function bagInteraction(){
-
+    // deselect hand item
     if(currItem == bagContent[bagItem]){
         currItem = "";
         document.getElementById("handLabel").innerHTML = "";
         document.getElementById("hand").innerHTML = "";
         return;
+    // select hand item
     }else{
         currItem = bagContent[bagItem];
         document.getElementById("handLabel").innerHTML = "Hand: ";
@@ -113,80 +96,19 @@ function bagInteraction(){
     }
 }
 
-
 // views
 var view = {
     
-    dungeon : function(d){
-        
-        switch(d){
-            case 0:
-            // init
-                option = 0;
-                currScene = "dungeon";
-                document.cookie = "currScene=dungeon";
-                document.getElementById("location").innerHTML = "Dungeon";
-                break;
-            case 10:
-            // interact
-                switch(option){
-                    case 0:
-                    // pile of ruins
-                        document.getElementById("info").innerHTML = "Here you fell through the ceiling.";
-                        return;
-                    case 1:
-                    // dirty sheet / tunnel
-                        if(dungeonOptions[1] == "a dirty sheet."){
-                        // remove sheet
-                            dungeonOptions[1] = "a tunnel.";
-                            document.cookie = "dungeonOptions[1]=a tunnel.";
-                            document.getElementById("view").innerHTML = dungeonOptions[1];
-                            document.getElementById("info").innerHTML = "You removed the sheet and found a tunnel.";
-                            return;
-                        }else if(dungeonOptions[1] == "a tunnel."){
-                        // move to dungeon2
-                            option = 0;
-                            currScene = "dungeon2";
-                            document.cookie = "currScene=dungeon2";
-                            document.getElementById("location").innerHTML = "Dungeon 2";
-                            document.getElementById("info").innerHTML = "You went through the tunnel to another Dungeon.";
-                            document.getElementById("view").innerHTML = dungeon2Options[option];
-                            return;
-                        }
-                        break;
-                    case 2:
-                    // door
-                        if(dungeonOptions[2] == "the door."){
-                            if(currItem == "key"){
-                                bagContent.splice(bagContent.indexOf("key"), 1);
-                                dungeonOptions[2] = "the opened Door.";
-                                document.cookie = "dungeonOptions[2]=the opened door.";
-                                document.cookie = "bagContent-=key"; //fix this
-                                document.getElementById("view").innerHTML = dungeonOptions[2];
-                                document.getElementById("info").innerHTML = "You opened the Door.";
-                                document.getElementById("handLabel").innerHTML = "";
-                                document.getElementById("hand").innerHTML = "";
-                                return;
-                            }else{
-                                document.getElementById("info").innerHTML = "The door is locked.";
-                                return;
-                            }
-                        }else if(dungeonOptions[2] == "the opened Door."){
-                            document.getElementById("view").innerHTML = "freedom!";
-                            document.getElementById("info").innerHTML = "Congratulations, you passed the test ;)";
-                            return;
-                        }
-                        break;
-                    case 3:
-                    // azruk
-                        document.getElementById("info").innerHTML = "Hi, I'm Azruk, the dungeon master!";
-                        return;
-                }
-                break;
-            default:
-            // change option
-                option += d;
-                break;
+    dungeon: function(v){
+        // init
+        if(v == 0){
+            option = 0;
+            currScene = "dungeon";
+            document.cookie = "currScene=dungeon";
+            document.getElementById("location").innerHTML = "Dungeon";
+        // change current view
+        }else{
+            option += v;
         }
         
         // cycle
@@ -196,58 +118,20 @@ var view = {
             option = dungeonOptions.length - 1;
         }
 
-        // print option
+        // print current view
         return document.getElementById("view").innerHTML = dungeonOptions[option];
     },
 
-    dungeon2 : function(d){
-        
-        switch(d){
-            case 0:
-            // init
-                option = 0;
-                currScene = "dungeon2";
-                document.cookie = "currScene=dungeon2";
-                document.getElementById("location").innerHTML = "Dungeon 2";
-                break;
-            case 10:
-            // interact
-                switch(option){
-                    case 0:
-                        document.getElementById("info").innerHTML = "It looks the way you feel right now.";
-                        return;
-                    case 1:
-                    // move back to dungeon1
-                        option = 0;
-                        currScene = "dungeon";
-                        document.cookie = "currScene=dungeon";
-                        document.getElementById("location").innerHTML = "Dungeon";
-                        document.getElementById("info").innerHTML = "You went back through the tunnel.";
-                        document.getElementById("view").innerHTML = dungeonOptions[option];
-                        return;
-                    case 2:
-                        document.getElementById("info").innerHTML = "You don't want to dig yourself out of the dungeon.";
-                        return;
-                    case 3:
-                    // key
-                        if(dungeon2Options.includes("a key.")){
-                            dungeon2Options[3] = "an empty place."
-                            bagContent.push("key");
-                            document.cookie = "dungeon2Options[3]=an empty place";
-                            document.cookie = "bagContent+=key"; //fix this
-                            document.getElementById("view").innerHTML = dungeon2Options[3];
-                            document.getElementById("info").innerHTML = "You put the key into your bag.";
-                            return;
-                        }else{
-                            document.getElementById("info").innerHTML = "It's nothing here anymore.";
-                            return;
-                        }   
-                }
-                break;
-            default:
-            // change option
-                option += d;
-                break;
+    dungeon2: function(v){
+        // init
+        if(v == 0){        
+            option = 0;
+            currScene = "dungeon2";
+            document.cookie = "currScene=dungeon2";
+            document.getElementById("location").innerHTML = "Dungeon 2";
+        // change current view
+        }else{
+            option += v;
         }
         
         // cycle
@@ -257,7 +141,96 @@ var view = {
             option = dungeon2Options.length - 1;
         }
 
-        // print option
+        // print current view
         return document.getElementById("view").innerHTML = dungeon2Options[option];
+    },
+};
+
+// interactions
+var interact = {
+    dungeon: function(){
+        switch(option){
+        case 0:
+            document.getElementById("info").innerHTML = "Here you fell through the ceiling.";
+            return;
+        // dirty sheet / tunnel
+        case 1:
+            // remove sheet
+            if(dungeonOptions[1] == "a dirty sheet."){
+                dungeonOptions[1] = "a tunnel.";
+                document.cookie = "dungeonOptions[1]=a tunnel.";
+                document.getElementById("view").innerHTML = dungeonOptions[1];
+                document.getElementById("info").innerHTML = "You removed the sheet and found a tunnel.";
+                return;
+            // move to dungeon2
+            }else if(dungeonOptions[1] == "a tunnel."){
+                option = 0;
+                currScene = "dungeon2";
+                document.cookie = "currScene=dungeon2";
+                document.getElementById("location").innerHTML = "Dungeon 2";
+                document.getElementById("info").innerHTML = "You went through the tunnel to another Dungeon.";
+                document.getElementById("view").innerHTML = dungeon2Options[option];
+                return;
+            }
+        // door
+        case 2:
+            if(dungeonOptions[2] == "the door."){
+                if(currItem == "key"){
+                    bagContent.splice(bagContent.indexOf("key"), 1);
+                    dungeonOptions[2] = "the opened Door.";
+                    document.cookie = "dungeonOptions[2]=the opened door.";
+                    document.cookie = "bagContent-=key"; //fix this
+                    document.getElementById("view").innerHTML = dungeonOptions[2];
+                    document.getElementById("info").innerHTML = "You opened the Door.";
+                    document.getElementById("handLabel").innerHTML = "";
+                    document.getElementById("hand").innerHTML = "";
+                    return;
+                }else{
+                    document.getElementById("info").innerHTML = "The door is locked.";
+                    return;
+                }
+            }else if(dungeonOptions[2] == "the opened Door."){
+                document.getElementById("view").innerHTML = "freedom!";
+                document.getElementById("info").innerHTML = "Congratulations, you passed the test ;)";
+                return;
+            }
+        case 3:
+            document.getElementById("info").innerHTML = "Hi, I'm Azruk, the dungeon master!";
+            return;
+        }
+    },
+    
+    dungeon2: function(){
+        switch(option){
+        case 0:
+            document.getElementById("info").innerHTML = "It looks the way you feel right now.";
+            return;
+        // move back to dungeon1
+        case 1:
+            option = 0;
+            currScene = "dungeon";
+            document.cookie = "currScene=dungeon";
+            document.getElementById("location").innerHTML = "Dungeon";
+            document.getElementById("info").innerHTML = "You went back through the tunnel.";
+            document.getElementById("view").innerHTML = dungeonOptions[option];
+            return;
+        case 2:
+            document.getElementById("info").innerHTML = "You don't want to dig yourself out of the dungeon.";
+            return;
+        // key
+        case 3:
+            if(dungeon2Options.includes("a key.")){
+                dungeon2Options[3] = "an empty place."
+                bagContent.push("key");
+                document.cookie = "dungeon2Options[3]=an empty place";
+                document.cookie = "bagContent+=key"; //fix this
+                document.getElementById("view").innerHTML = dungeon2Options[3];
+                document.getElementById("info").innerHTML = "You put the key into your bag.";
+                return;
+            }else{
+                document.getElementById("info").innerHTML = "It's nothing here anymore.";
+                return;
+            }   
+        }
     },
 };
